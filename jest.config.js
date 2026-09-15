9371455c5726f45576ddf5df48f3d226303fb6e9
pedro <ph3669023@gmail.com>
2026-09-15 19:24:10 -0300

    fix: separa tsconfig de producao e de testes

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 const { createDefaultPreset } = require("ts-jest");
 
-const tsJestTransformCfg = createDefaultPreset().transform;
+const tsJestTransformCfg = createDefaultPreset({
+  tsconfig: "tsconfig.test.json"
+}).transform;
 
-/** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
   transform: {
